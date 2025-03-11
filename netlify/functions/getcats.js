4a0b81ec99ef77eef9e71eecc2688028706d4be6
Leyla Carmona <leylacarmona2010@hotmail.es>
2025-03-11 13:08:28 -0600

    $git commit

--- netlify/functions/getcats.js
+++ netlify/functions/getcats.js
@@ -1,10 +1,9 @@
-// netlify/functions/getCats.js
-const fetch = require('node-fetch');  // Necesitarás instalar node-fetch
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const url = process.env.CAT_API_URL;
     const apiKey = process.env.CAT_API_KEY;
-    
+
     try {
         const response = await fetch(`${url}?api_key=${apiKey}`);
         const data = await response.json();
@@ -15,7 +14,7 @@ exports.handler = async function(event, context) {
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error al obtener datos' }),
+            body: JSON.stringify({ error: 'Error al obtener los datos de los gatos' }),
         };
     }
 };
